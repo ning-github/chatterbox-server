0f7fad256b1fb7629cb6bff74e3c1024be300d4d
Ning Xia <nxiamail@gmail.com>
2015-06-04 19:59:12 -0700

    adding client files of backbonfied chatterbox

---
+++ client/scripts/app.js
@@ -0,0 +1,85 @@
+// /////////////////////////////////////////
+// BACKBONE.JS IMPLEMENTATION
+// /////////////////////////////////////////
+var Message = Backbone.Model.extend({
+  url: 'http://127.0.0.1:3000/classes/chatterbox/',
+  defaults: {
+    username: 'Santa'
+  }
+});
+
+var Messages = Backbone.Collection.extend({
+  model: Message,
+  url: 'http://127.0.0.1:3000/classes/chatterbox',
+
+  loadMessages: function () {
+    this.fetch();
+  },
+
+  parse: function (response, options) {
+    var output = [];
+
+    for (var i = 0; i < response.results.length; i++) {
+      output.push(response.results[i]);
+    };
+
+    return output;
+  }
+});
+
+var MessageView = Backbone.View.extend({
+  model: Message,
+
+  template: _.template(['<li class="well">',
+        '<div class="username"><%-username%></div>',
+        '<div class="msgText"><%-text%></div>',
+        '<div><%-roomname%></div>',
+      '</li>',].join('')),
+
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+});
+
+var MessagesView = Backbone.View.extend({
+  initialize: function () {
+    this.collection.on('sync', this.render, this);
+    this.onScreenMessages = {};
+  },
+
+  render: function () {
+    // clears the feed (we can do this without flashing since line 47 syncs)
+    this.$el.html('');
+    // render
+    this.collection.forEach(this.renderMessage, this);
+  },
+
+  renderMessage: function(item){
+    var newMessageView = new MessageView({model:item});
+    this.$el.prepend(newMessageView.render());
+  }
+});
+
+var SubmitView = Backbone.View.extend({
+
+  events: {
+    'click .submit': 'doSubmit'
+  },
+
+  doSubmit: function(e){
+    e.preventDefault();
+
+    var $text = this.$('#message');
+    var $roomname = this.$('#roomname');
+    var data = {
+      username: window.location.search.substr(10),
+      text: $text.val(),
+      roomname: $roomname.val()
+    };
+    this.collection.create(data);
+    $text.val('');
+    $roomname.val('');
+  },
+
+});
